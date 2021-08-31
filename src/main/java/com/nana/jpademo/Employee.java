aec9b0b5d22217a59ba31b3b3d310a6b09eb222a
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-31 23:10:50 +0000

    added the tostring method

--- src/main/java/com/nana/jpademo/Employee.java
+++ src/main/java/com/nana/jpademo/Employee.java
@@ -39,6 +39,14 @@ public class Employee {
         this.lastName = lastName;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public Long getId() {
         return id;
     }
@@ -47,12 +55,16 @@ public class Employee {
         this.id = id;
     }
 
+
+
     @Override
     public String toString() {
         return "Employee{" +
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                "title='" + title + '\'' +
                 '}';
+
     }
 }
